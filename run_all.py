f7c0ff950963bda0ad4b996896c702fd6084344a
Margarida <marghrid@hotmail.com>
2020-02-17 10:26:48 +0000

    run_all_update

--- run_all.py
+++ run_all.py
@@ -1,6 +1,5 @@
 import glob
 import subprocess
-import os
 
 instances = []
 for file in glob.glob("instances/strings/*.txt"):
@@ -9,23 +8,26 @@ commands = [["runsolver", "-W", "60", "python3", "validate.py", "-f", instance] 
 
 processes = []
 
+instance_times = {}
+
 for command in commands:
-    process = subprocess.Popen(command,
-                               stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    processes.append((process, command))
-    
-for task in processes:
-    process = task[0]
-    instance = task[1][-1]
-    print("\n=====  " + instance + "  =====")
-    try:
-        process.wait(timeout=120)
-    except subprocess.TimeoutExpired:
-        print("timeout")
-        continue
-    po, pe = process.communicate()
-    po = str(po, encoding ='utf-8').splitlines()
-    pe = str(pe, encoding ='utf-8').splitlines()
-    for l in po + pe:
-        if "Real time" in l:
-            print(l)
+    times = []
+    print("\n=====  " + command[-1] + "  =====")
+    for i in range(4):
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+
+        po, pe = process.communicate()
+        po = str(po, encoding ='utf-8').splitlines()
+        pe = str(pe, encoding ='utf-8').splitlines()
+        for l in po + pe:
+            if "Real time" in l:
+                time = l.replace("Real time (s):", " ", 1)
+                time = float(time)
+                times.append(time)
+                print(time, "s")
+    avg_time = sum(times) / len(times)
+    print("avg time:", avg_time, "s")
+    instance_times[command[-1]] = avg_time
+
+print(instance_times)
