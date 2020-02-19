333738f6db730a1e5d909156dd7a2415665c9425
Margarida <marghrid@hotmail.com>
2020-02-19 15:45:23 +0000

    run_all registers number of enumerated programs

--- run_all.py
+++ run_all.py
@@ -1,19 +1,19 @@
 import glob
+import re
 import subprocess
 
 instances = []
 for file in glob.glob("instances/strings/*.txt"):
     instances.append(file)
 commands = [["runsolver", "-W", "60", "python3", "validate.py", "-f", instance] for instance in instances]
-
 processes = []
-
 instance_times = {}
 
 for command in commands:
     times = []
+    instructions = []
     print("\n=====  " + command[-1] + "  =====")
-    for i in range(8):
+    for i in range(4):
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         processes.append(process)
     
@@ -22,16 +22,23 @@ for command in commands:
         po, pe = process.communicate()
         po = str(po, encoding ='utf-8').splitlines()
         pe = str(pe, encoding ='utf-8').splitlines()
+        time = -1
+        enumerated = -1
         for l in po + pe:
-
             if "Maximum wall clock time exceeded:" in l:
                 print("timeout")
                 break
             if "Real time" in l:
                 time = l.replace("Real time (s):", " ", 1)
                 time = float(time)
-                times.append(time)
-                print(time, "s")
+            if "attempts" in l:
+                regex = "(\\d+) attempts"
+                bla = int(re.search(regex, l)[1])
+                enumerated = bla
+        times.append(time)
+        print(time, "s")
+        print("enumerated", bla)
+
     processes = []
     if len(times) > 0:
         avg_time = sum(times) / len(times)
