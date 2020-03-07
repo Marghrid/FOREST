0a0470fc7c8ca48be8c4e987fc6eb826a4e5e000
Marghrid <marghrid@hotmail.com>
2020-03-07 22:40:15 +0100

    Tester now kills remaining processes of instances that timeout.

--- scripts/run.py
+++ scripts/run.py
@@ -7,28 +7,18 @@ from tester import Tester
 def main():
 
     parser = argparse.ArgumentParser(description='Validations Synthesizer tester')
-    parser.add_argument('directories', type=str, metavar="dir", nargs='+', help='Directories with')
-    parser.add_argument('-p', '--processes', type=int, help='Number of processes')
-    parser.add_argument('-r', '--run-each', type=int, help='Number of times to run each instance')
-    parser.add_argument('-t', '--timeout', type=int, help='Timeout in seconds')
-    parser.add_argument('-o', '--out', action='store_true', help='Show output')
+    parser.add_argument('directories', type=str, metavar="dir", nargs='+', help='Directories with instances')
+    parser.add_argument('-p', '--processes', metavar="P", type=int, help='Number of processes. Default: 1.', default=1)
+    parser.add_argument('-r', '--run-each', metavar="R", type=int,
+                        help='Number of times to run each instance. Default: 1.', default=1)
+    parser.add_argument('-t', '--timeout', metavar="T", type=int, help='Timeout in seconds. Default: 120.', default=120)
+    parser.add_argument('-o', '--out', action='store_true', help='Show output. Default: False.', default=False)
     args = parser.parse_args()
 
-    # defaults:
-    num_processes=1
-    run_each=1
-    timeout=120
-    runsolver=False
-    show_output=False
-
-    if args.processes:
-        num_processes = args.processes
-    if args.run_each:
-        run_each = args.run_each
-    if args.timeout:
-        timeout = args.timeout
-    if args.out:
-        show_output = args.show_output
+    num_processes = args.processes
+    run_each = args.run_each
+    timeout = args.timeout
+    show_output = args.out
 
     tester = Tester(args.directories, num_processes, run_each, timeout, False, show_output)
     tester.test()
