5c06746b9ef1cb0054971c560a30ec130d593600
Marghrid <margaridaacferreira@tecnico.ulisboa.pt>
2021-01-01 09:10:58 +0000

    TACAS21 run_all.sh script.

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,40 @@
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e multitree benchmarks/ |& tee out_multitree_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -i 5 -v 5    benchmarks/ |& tee out_5ofeach_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -i 10 -v 10  benchmarks/ |& tee out_10ofeach_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e ktree     benchmarks/ |& tee out_ktree_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e lines     benchmarks/ |& tee out_lines_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e funny     benchmarks/ |& tee out_funny_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e multitree --nopruning benchmarks/ |& tee out_nopruning_macf_15.txt
-python -u scripts/run_tests.py -t 3600 -o -p 16 -e ktree --nopruning benchmarks/ |& tee out_nopruning_macf_15.txt
+#!/bin/bash
+timeout=$1
+log_prefix=${2-log}
 
+if [ $# -eq 0 ]; then
+    echo "Usage: ${0} TIMEOUT_IN_SECONDS [LOG_PREFIX]".  
+    exit 1
+fi
+
+for n in "mt" "np" "dy" "kt" "li" "05" "10"
+do
+	if [[ -d "${log_prefix}_${n}" ]]
+	then
+	    echo "${log_prefix}_${n} exists. Please delete it or define another log prefix."
+	    exit 1
+	fi
+	# mkdir  ${log_prefix}_${n} 
+done
+
+
+# default setting:
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1            --log ${log_prefix}_mt  benchmarks/
+
+# Multi-tree encoding without pruning:
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -e multitree --nopruning --log ${log_prefix}_np  benchmarks/
+
+# Dynamic-only muti-tree encoding:
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -e dynamic --log ${log_prefix}_dy  benchmarks/
+
+# k-tree encoding:
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -e ktree   --log ${log_prefix}_kt  benchmarks/
+
+# Line-based encoding
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -e lines   --log ${log_prefix}_li  benchmarks/
+
+# 5 examples of each kind only
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -m 5       --log ${log_prefix}_05  benchmarks/
+
+# 10 examples of each kind only
+python -u scripts/run_benchmarks.py -t ${timeout} -p 1 -m 10      --log ${log_prefix}_10  benchmarks/
