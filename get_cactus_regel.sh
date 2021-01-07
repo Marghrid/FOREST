38de6bf217c89c67820661d327f44e3d03ab1a5f
Marghrid <margaridaacferreira@tecnico.ulisboa.pt>
2021-01-07 02:26:20 +0000

    :bug:

--- get_cactus_regel.sh
+++ get_cactus_regel.sh
@@ -2,8 +2,8 @@
 log_prefix=${1-log}
 
 # REGEL
-python3 scripts/make_table.py --rank-regel -r ${log_prefix}_rg  ${log_prefix}_mt
+python3 scripts/make_table.py --rank-regel -r ${log_prefix}_rg  ${log_prefix}_mt > cactus_3600_rg.csv
 
 # REGEL PBE
-python3 scripts/make_table.py --rank-regel -r ${log_prefix}_rgp  ${log_prefix}_mt
+python3 scripts/make_table.py --rank-regel -r ${log_prefix}_rgp  ${log_prefix}_mt > cactus_3600_rgp.csv
 
